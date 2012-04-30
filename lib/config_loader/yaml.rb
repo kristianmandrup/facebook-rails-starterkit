0c62344e299d291b596a8cb0629c2f67bac650b0
Kristian Mandrup <kmandrup@gmail.com>
2012-04-30 13:42:17 +0200

    better yaml config

--- lib/config_loader/yaml.rb
+++ lib/config_loader/yaml.rb
@@ -1,33 +1,31 @@
 module ConfigLoader
   class Yaml
-		def initialize path, file_name, root = nil
-			@path = path
+  	attr_reader :path, :file_name, :file_path, :root, :hash
 
-			file_name += '.yml' unless file_name =~ /\.ya?ml/
-			@file_name = file_name  
+		def initialize file_path, root = nil
+			@file_path = file_path
+			@path 		 = File.dirname file_path
+			@file_name = File.basename file_path
+			@root = root
 			
 			hash = Hashie::Mash.new yaml
-			root ? hash.send(root) : hash
+			@hash = root ? hash.send(root) : hash
 		end
   	
 		protected
 
 		def yaml
-			@yaml ||= YAML::load File.open(file_path)
+			@yaml ||= YAML::load File.open(config_file_path)
 		end
 
-  	def config_path
-			@config_path ||= File.join Rails.root, 'config'
-		end
-
-    def file_path
-    	@file_path = File.join config_path, path, file_name
+    def config_file_path
+    	@config_file_path ||= Rails.root.join('config', file_path)
     end
 	end
 
 	module Delegator				
 	  def method_missing(m, *args, &block)
-	  	raise 'A #config method must be defined in the container for ConfigLoader::Delegator, for it to delegate to #config' unless respond_to?(:config)
+	  	raise "A #config method must be defined in the container for ConfigLoader::Delegator, for it to delegate to #config: delegation attempted with #{m}" unless self.respond_to?(:config)
 	    config.send(m)
 	  end
 	end
